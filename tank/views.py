a0e4705c6994bb78a523e97c09c8ab424bf2af03
Roshan <roshanb1943@gmail.com>
2024-05-01 23:43:34 +0530

    Revert "chartjs configured"
    This reverts commit 348175f0be2ad4357c2ce04ccd0dbce3f4c64486.

--- tank/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def tank(request):
-    return render(request, 'tank/tank.html', {'title': 'Tank Page'})
-
-def home(request):
-    return render(request, 'tank/home.html', {'title': 'Home Page'})
